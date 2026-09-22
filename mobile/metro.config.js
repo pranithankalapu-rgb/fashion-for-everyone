6357d1646ffa5ebe007e8a7970d09671762cc802
pranithankalapu-rgb <pranith.ankalapu@gmail.com>
2026-09-22 23:59:59 +0530

    fix(mobile): use dynamic project paths in metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,13 +4,11 @@ const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 const projectRoot = __dirname;
-const realRoot = path.resolve('c:/Users/prani/OneDrive/Desktop/fashion-for-everyone/mobile');
 
 config.watchFolders = [
   projectRoot,
-  realRoot,
-  path.join(realRoot, 'node_modules'),
   path.join(projectRoot, 'node_modules'),
 ];
 
 module.exports = config;
+
